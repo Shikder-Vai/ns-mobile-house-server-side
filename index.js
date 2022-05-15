378ad6c0aeaf45aed514aaa440097ffc4543354d
unknown <Shikder-Vai>
2022-05-15 16:46:05 +0600

    structur added

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+function verifyJWT(req, res, next) {
+  const tokenInfo = req.headers.authorization;
+
+  if (!tokenInfo) {
+    return res.status(401).send({ message: "Unouthorize access" });
+  }
+  const token = tokenInfo.split(" ")[1];
+  jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    } else {
+      req.decoded = decoded;
+      next();
+    }
+  });
+}
